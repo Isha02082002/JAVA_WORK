ee4dcf05f27d866ec7797ae5aabb8a1536f7cc89
isha <isharai8584@gmail.com>
2024-10-19 18:42:52 +0530

    first commit of java program

---
+++ Cube.java
@@ -0,0 +1,16 @@
+
+
+class Cube{
+    public static void main(String arg[]){
+        int  n , s , c ;
+        //declare :
+        n=5 ;
+        s= n*n;
+        c=n*n*n;
+        //operation :
+        System.out.println( " square :" +s);
+        System.out.println("cube : " +c);
+
+
+    }
+}
