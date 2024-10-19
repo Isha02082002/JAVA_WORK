d3f0be235b6c7c71aa6d47fc839fd5e8995b594f
isha <isharai8584@gmail.com>
2024-10-19 18:48:19 +0530

    second program of java

---
+++ Rect.java
@@ -0,0 +1,14 @@
+class Rect{
+      public static void main(String args[]){
+        int l,b,ar,pr; //step 1 declare
+        l=4;
+        b=5;
+        //step2 : initialize
+        ar=l*b;
+        pr=2*(l+b);
+        //step 3  :operation
+        System.out.println("Area :" +ar);
+        System.out.println("Paremeter :"+pr);
+        
+      }
+}
